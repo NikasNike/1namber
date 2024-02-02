99697fdb14f2ca8ed36c5dc55d6d18a8301eaa41
nikita <nikasnike759@gmail.com>
2024-02-02 10:02:05 +0300

    финал

--- def.py
+++ def.py
@@ -1,42 +1,109 @@
+
+import openpyxl
+from openpyxl import load_workbook
+from datetime import datetime
+from datetime import date
+import pandas as pd
+import csv
+
+wb = openpyxl.load_workbook('example.xlsx')
+wb.sheetnames
+sheet = wb.active
+ws = wb['Первый лист']
 command = 'nul'
 x = 0
+
 while command != 'exit':
-    command = input("Введите что-нибудь, чтобы проверить это: ")
+    command = input("Введите команду, или 'info': ")
 
-    def stop_service():
-        print(x + 1)
+    def save():
+        wb.save('example.xlsx')
+        print("Заметка сохранена")
+        col = csv.writer(open("tt.csv", 
+                      'w',  
+                      newline="")) 
+        for r in ws.rows: 
+            col.writerow([cell.value for cell in r])  
+        
+    
+    def info():
+        print("'save' - сохранить изминения \
+            'read' - прочитать данные \
+            'add' - добавить данные \
+            'edit' - изменить данные \
+            'exit' - выйти без сохранения\
+            'delite' - удалить данные")    
+    
+    def read():
+                print("Что бы вывести все введите '0'")
+                z1 = input("Введите номер заметки или введите дату в формате 'mm-dd': ")
+                if  z1.isdigit() == True:
+                    z1 = int(z1)
+                    if z1 == 0:
+                        for row in ws.iter_rows(min_row=1, max_col=4, values_only=True):
+                            if row[0] == None:
+                                return
+                            else:
+                                print(row)
+                    elif isinstance(z1, (int)) == True:
+                        for row in ws.iter_rows(min_row=z1, max_row=z1, max_col=4, values_only=True):
+                            if row[0] == None:
+                                print("такйо заметки нет")
+                            else:
+                                print(row)
+                else:
+                     for row in ws.iter_rows(min_row=1, max_col=4, values_only=True):
+                         zz = row[3]
+                         if zz[5:10] == z1:
+                             print(row)
+                         else:
+                            print("Такой заметки нет")
+                            return
+    
+    def add():
+        for row in ws.iter_rows(min_row=1, max_col=1, values_only=True):
+           i = 0
+           
+        if row[0] is None or isinstance(row[0], (str)) == True:
+            izi = 0
+        else:
+            print(type(row[0]))
+            izi = (row[0])
+        now = datetime.now()
+        today = date.today()
+        d0 = input("Введите заголовок заметки: ")
+        d = input("Введите тело заметки: ")
+        ws.append({1 : (izi + 1), 2 : d0, 3 : d, 4 : "{}  {}:{}".format(today, now.hour, now.minute)})
+        print("Заметка успешно сохранена")
+        
+    def edit():
+        d1 = int(input("Ведите какую строку изменить: "))
+        d12 = input("Ведите новые данные: ")
+        now = datetime.now()
+        ws.cell(row=d1, column=3, value=d12)
+        ws.cell(row=d1, column=4, value="{}.{}.{}  {}:{}".format(now.day, now.month, now.year, now.hour, now.minute))
+        print("Заметка успешно изменена")
 
-    def restart_service():
-        return(x + 2)
+    def delite():
+        d13 = int(input("введите какую строку удалить: "))
+        ws.delete_rows(idx = d13, amount=1)
+        print("Заметка успешно удалена")
 
-    def start_service():
-        return(x + 3)
-
-    def set_reg_key():
-        return(x + 4)
-
-    def disable_agent_adapter_net_int():
-        return(x + 5)
-
-    def enable_agent_adapter_net_int():
-        return(x + 6)
     try:
         method = {
-            'stop': stop_service,
-            "restart-service": restart_service(),
-            "start-service": start_service(),
-            "set-reg-key": set_reg_key(),
-            "disable-agent-adapter-net-int": disable_agent_adapter_net_int(),
-            "enable-agent-adapter-net-int": enable_agent_adapter_net_int()
-
+            'save': save,
+            'read': read,
+            'add': add,
+            'edit': edit,
+            'delite': delite,
+            'info': info,
+            'exit': exit
         }
-        method[command]()
-    except LookupError:
-        print("исключение")
-    else:
-        print("успех")
+        method[command.lower()]()
 
-    
-    
+    except LookupError:
+        print("Неверная команда")
+    else:
+        x = 0
+
 print("вы вышли((()))")
-    
